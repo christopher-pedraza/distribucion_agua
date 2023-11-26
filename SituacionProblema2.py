3b700ff10cc9eff06448458ab4c3db40c11d7c26
Carolina Lara <a01368710@tec.mx>
2023-11-26 01:21:09 -0600

    frescura agua mejorado

--- SituacionProblema2.py
+++ SituacionProblema2.py
@@ -51,23 +51,33 @@ def problema_3(carpeta):
 
 
 def problema_4(carpeta):
-    grafo_FOS, new_nodes_FOS = Graph.create_graph("grafos/FOS.txt")
     Graph.crear_sector(grafo_FOS)
-    FrescuraAgua.max_delay_per_sector(grafo_FOS, f"resultados/{carpeta}/resultado_FrescuraAgua_FOS.txt")
-
-    grafo_HAN, new_nodes_HAN = Graph.create_graph("grafos/HAN.txt")
+    resultado_FOS = FrescuraAgua.max_delay_per_sector(grafo_FOS)
+    FrescuraAgua.guardar_resultados_en_archivo(
+        f"resultados/{carpeta}/resultado_FrescuraAgua_FOS.txt", resultado_FOS
+    )
     Graph.crear_sector(grafo_HAN)
-    FrescuraAgua.max_delay_per_sector(grafo_HAN, f"resultados/{carpeta}/resultado_FrescuraAgua_HAN.txt")
-
-    grafo_NYT, new_nodes_NYT = Graph.create_graph("grafos/NYT.txt")
+    resultado_HAN = FrescuraAgua.max_delay_per_sector(grafo_HAN)
+    FrescuraAgua.guardar_resultados_en_archivo(
+        f"resultados/{carpeta}/resultado_FrescuraAgua_HAN.txt", resultado_HAN
+    )
     Graph.crear_sector(grafo_NYT)
-    FrescuraAgua.max_delay_per_sector(grafo_NYT, f"resultados/{carpeta}/resultado_FrescuraAgua_NYT.txt")
-
-    grafo_PES, new_nodes_PES = Graph.create_graph("grafos/PES.txt")
+    resultado_NYT = FrescuraAgua.max_delay_per_sector(grafo_NYT)
+    FrescuraAgua.guardar_resultados_en_archivo(
+        f"resultados/{carpeta}/resultado_FrescuraAgua_NYT.txt", resultado_NYT
+    )
     Graph.crear_sector(grafo_PES)
-    FrescuraAgua.max_delay_per_sector(grafo_PES, f"resultados/{carpeta}/resultado_FrescuraAgua_PES.txt")
+    resultado_PES = FrescuraAgua.max_delay_per_sector(grafo_PES)
+    FrescuraAgua.guardar_resultados_en_archivo(
+        f"resultados/{carpeta}/resultado_FrescuraAgua_PES.txt", resultado_PES
+    )
+
+def problema_7():
+    Graph.save_graph_to_file(grafo_PES, "PES")
 
 
+def problema_7():
+    Graph.save_graph_to_file(grafo_PES, "PES")
 
 
 
